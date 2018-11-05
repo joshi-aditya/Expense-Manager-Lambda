e9b7680ab7852663855201c764b63efd0ea88fe5
aditya-joshi-neu <joshi.adit@northeastern.edu>
2018-11-04 20:16:05 -0500

    added sample lambda file

---
+++ webapp/lambda.js
@@ -0,0 +1,52 @@
+exports.handler = function (event, context, callback) {
+
+    // Load the SDK for JavaScript
+    var AWS = require('aws-sdk');
+    // Set the region 
+    AWS.config.update({ region: 'us-east-1' });
+
+    var message = event.Records[0].Sns.Message;
+    console.log('Message received from SNS:', message);
+
+    //Added to make an entry to Dynamo DB
+    // Create the DynamoDB service object
+    var ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
+    const uuidv1 = require('uuid/v1');
+    var token = uuidv1();
+
+    //First check if the email is present in the table
+    var params = {
+        TableName: 'Credentials',
+        Key: {
+            'token': { S: "001" },
+        }
+    };
+
+    // Call DynamoDB to read the item from the table
+    ddb.getItem(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.Item);
+        }
+    });
+
+    var params = {
+        TableName: 'Credentials',
+        Item: {
+            'token': { S: token },
+            'username': { S: message },
+        }
+    };
+
+    // Call DynamoDB to add the item to the table
+    ddb.putItem(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
+
+    callback(null, "Success");
+};
