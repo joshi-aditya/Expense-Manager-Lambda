e36b3601bfb14225cfc037f3d05ea6760c069ace
sskawitkar <43149937+sskawitkar@users.noreply.github.com>
2018-11-11 14:13:39 -0500

    Review for email notification (#2)
    * Added adityas changes
    
    * Latest changes
    
    * Email notification done

--- webapp/lambda.js
+++ webapp/lambda.js
@@ -1,77 +1,112 @@
 exports.handler = function(event, context, callback) {
     
-        // Load the SDK for JavaScript
-        var AWS = require('aws-sdk');
-        // Set the region 
-        AWS.config.update({region: 'us-east-1'});
-        
-        var message = event.Records[0].Sns.Message;
-        console.log('Message received from SNS:', message); 
-        
-        //Added to make an entry to Dynamo DB
-        // Create the DynamoDB service object
-        var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-        const uuidv1 = require('uuid/v1');
-        var token  = uuidv1();
-        
-        var expiryTime = 1 * 60; //20 minutes
-        var ttl = (new Date).getTime() + expiryTime;
-        
-        const docClient = new AWS.DynamoDB.DocumentClient();
-        
-        //First check if the email is present in the table
-        var params = {
-                ExpressionAttributeNames: {"#username": "username"},
-                ExpressionAttributeValues: {":username": message},
-                FilterExpression: "#username = :username and attribute_not_exists(Removed)",
-                Limit: 20,
-                TableName: "Credentials"
-            };
-            
-        docClient.scan(params, function(err, data) {
-                if (err) {
-                    console.log("Error occured while fetching the record");
-                } else if (data.Items.length == 0) {
-                    
-                   addCredentials();
-                    
-                } else {
-                    
-                    console.log("Current Time : " + (new Date).getTime());
-                    console.log("TTL is : "+ data.Items[0].ttl);
-                    if((data.Items[0].ttl - (new Date).getTime()) > expiryTime)
-                    {
-                        console.log("Password reset link expired");
-                        addCredentials();
-                    }
-                    else
-                    { 
-                        console.log("Email already sent!");
-                    }
-                }
-            });
-            
-        function addCredentials()
-        {
-           var params2 = {
-                          TableName: 'Credentials',
-                          Item: {
-                            'token' : {S: token},
-                            'username' : {S: message},
-                            'ttl' : {S: "" + ttl}
-                          }
-                        };
-                        
-            // Call DynamoDB to add the item to the table
-            ddb.putItem(params2, function(err, data) {
-              if (err) {
-                console.log("Error", err);
-              } else {
-                console.log("Success", data);
-              }
-            });
-        }
-        
-        callback(null, "Success");
-    };
+    // Load the SDK for JavaScript
+    var AWS = require('aws-sdk');
+    //  var nodemailer = require('nodemailer');
+    var ses = new AWS.SES();
+    // Set the region 
+    AWS.config.update({region: 'us-east-1'});
     
+    var message = event.Records[0].Sns.Message;
+    console.log('Message received from SNS:', message); 
+    
+    //Added to make an entry to Dynamo DB
+    //Create the DynamoDB service object
+    var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+    const uuidv1 = require('uuid/v1');
+    var token  = uuidv1();
+    
+    var expiryTime = 1 * 60; //20 minutes
+    var ttl = (new Date).getTime() + expiryTime;
+    
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    
+    //First check if the email is present in the table
+    var params = {
+            ExpressionAttributeNames: {"#username": "username"},
+            ExpressionAttributeValues: {":username": message},
+            FilterExpression: "#username = :username and attribute_not_exists(Removed)",
+            Limit: 20,
+            TableName: "Credentials"
+        };
+        
+    docClient.scan(params, function(err, data) {
+            if (err) {
+                console.log("Error occured while fetching the record");
+            } else if (data.Items.length == 0) {
+                
+               addCredentials();     
+               sendEmail();
+                
+            } else {
+                
+                console.log("Current Time : " + (new Date).getTime());
+                console.log("TTL is : "+ data.Items[0].ttl);
+                if((data.Items[0].ttl - (new Date).getTime()) > expiryTime)
+                {
+                    console.log("Password reset link expired");
+                    addCredentials();
+                    sendEmail();
+                }
+                else
+                { 
+                    console.log("Email already sent!");                                              
+                }
+            }
+        });
+        
+    function addCredentials()
+    {
+       var params2 = {
+                      TableName: 'Credentials',
+                      Item: {
+                        'token' : {S: token},
+                        'username' : {S: message},
+                        'ttl' : {S: "" + ttl}
+                      }
+                    };
+                    
+        // Call DynamoDB to add the item to the table
+        ddb.putItem(params2, function(err, data) {
+          if (err) {
+            console.log("Error", err);
+          } else {
+            console.log("Success", data);
+          }
+        });
+    }
+    
+    function sendEmail(){
+          var eParams = {
+                            Destination: {
+                            ToAddresses: [message]
+                        },
+                        Message: {
+                            Body: {
+                                Text: {
+                                    Data: "Your reset password link is : "
+                                }
+                            },
+                            Subject: {
+                                Data: "Ses Test Email"
+                            }
+                        },
+                        Source: "donotreply@no-reply.csye6225-fall2018-kawitkars.me"
+                    };
+                console.log('===SENDING EMAIL===');
+                var email = ses.sendEmail(eParams, function(err, data){
+                    if(err) {
+                       console.log(err);                       
+                       context.fail(err);
+                    }
+                    else {
+                        console.log("===EMAIL SENT===");
+                        console.log("EMAIL CODE END");
+                        console.log('EMAIL: ', email);
+                        console.log(data);
+                        context.succeed(event);
+                    }
+                    
+                });                                         
+    }
+};
