4fdab4bc9d962f4822e654ed29c3f9b76b0d1bff
aditya-joshi-neu <43189825+aditya-joshi-neu@users.noreply.github.com>
2018-11-10 22:29:13 -0500

    Debugging travis

--- upload_lambda.sh
+++ upload_lambda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
 ls -al
-aws lambda update-function-code \
---zip-file=fileb://codedeploy_artifact/*.zip \
---region=us-east-1 \
---function-name=HelloWorld
+# aws lambda update-function-code \
+# --zip-file=fileb://codedeploy_artifact/*.zip \
+# --region=us-east-1 \
+# --function-name=HelloWorld
