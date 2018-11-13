5988dca28d7fd15ad963de7ec25a2e6c7581391e
aditya-joshi-neu <joshi.adit@northeastern.edu>
2018-11-12 21:36:21 -0500

    Added stack to create lambda

--- infrastructure/aws/cloudformation/csye6225-lambda-aws-cf-create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-lambda-aws-cf-create-serverless-stack.sh
@@ -2,14 +2,14 @@ read -p 'Enter stack name: ' STACK_NAME
 echo $STACK_NAME
 
 file_name="csye6225-lambda-*.zip"
-dynamodb_table=Credentials
-function=HelloWorld
+function=ResetPassword
 domain_name=$(aws route53 list-hosted-zones --query 'HostedZones[0].Name' --output text)
 bucket_name="lambda."$domain_name"csye6225.com"
+lambda_role=$(aws iam get-role --role-name LambdaRole --query Role.Arn --output text)
 
 STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
---template-body file://csye6225-lambda-aws-cf-cicd.json \
---parameters ParameterKey=BucketName,ParameterValue=${bucket_name} ParameterKey=FunctionName,ParameterValue=${function} ParameterKey=FileName,ParameterValue=${file_name} \
+--template-body file://csye6225-lambda-aws-cf-serverless.json \
+--parameters ParameterKey=BucketName,ParameterValue=${bucket_name} ParameterKey=FunctionName,ParameterValue=${function} ParameterKey=LambdaRole,ParameterValue=${lambda_role} ParameterKey=FileName,ParameterValue=${file_name} \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM \
 | jq -r .StackId \
