f1e0652545ae1cb6da0fc001e25b25a7e097e5d1
aditya-joshi-neu <43189825+aditya-joshi-neu@users.noreply.github.com>
2018-11-14 22:44:02 -0500

    Assignment07 08 (#10)
    * CICD stack creation
    
    * corrected travis file
    
    * changes in travis.yml
    
    * changes in travis.yml
    
    * added sample lambda file
    
    * travis changes
    
    * Updated the lambda file
    
    * Added Lambda function with TTL checks for password reset calls (#1)
    
    * Updated travis file
    
    * Travis update
    
    * Travis changes
    
    * changed appspec
    
    * Changed appspec version
    
    * Modified CICD stack
    
    * Travis changes to upload lambda
    
    * Updated policies
    
    * Changes to upload zip
    
    * Debugging travis
    
    * Changes in travis
    
    * Review for email notification (#2)
    
    * Added adityas changes
    
    * Latest changes
    
    * Email notification done
    
    * Testing file upload
    
    * Testing file upload
    
    * Checking AWS Configuration
    
    * Giving permissions
    
    * Updating permissions
    
    * updated permissions
    
    * Configuring AWS
    
    * Updated scripts with aws configure
    
    * Added LambdaSESPolicy
    
    * Added reset link and fixed issues related to ttl (#3)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * testing if else
    
    * debugging
    
    * Fixed error
    
    * debugging
    
    * Added serverless stack
    
    * Updating topic name field
    
    * debugging travis build
    
    * corrected sns topic name
    
    * debugging
    
    * debugging
    
    * updated travis
    
    * fixed topic name
    
    * Fixed errors
    
    * Updated the logic of ttl  (#4)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * Added changes to the lambda function ttl logic
    
    * Assignment07 08 (#5)
    
    * Added the lambda with TTL functionality
    
    Added the lambda with TTL functionality
    
    * Reverting this change
    
    * added environment variable and modified memory size
    
    * Added edge case for ttl (#6)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * Added changes to the lambda function ttl logic
    
    * Added changes to the ttl to save it as current time plus 20 minutes
    
    * Handle edge case for ttl (#7)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * Added changes to the lambda function ttl logic
    
    * Added changes to the ttl to save it as current time plus 20 minutes
    
    * Fixed the latest ttl issue to handle an edge case
    
    * Added 20 minutes for timeout (#8)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * Added changes to the lambda function ttl logic
    
    * Added changes to the ttl to save it as current time plus 20 minutes
    
    * Fixed the latest ttl issue to handle an edge case
    
    * Set the timeout to 20 minutes
    
    * 20 minutes (#9)
    
    * Added Lambda function with TTL checks for password reset calls
    
    * Updated the logic to check the ttl
    
    * Updated the Sender email
    
    * Added the reset email link in the lambda function
    
    * Updated the domain name and fixed issue related to ttl and reset link
    
    * Added changes to the lambda function ttl logic
    
    * Added changes to the ttl to save it as current time plus 20 minutes
    
    * Fixed the latest ttl issue to handle an edge case
    
    * Set the timeout to 20 minutes

---
+++ infrastructure/aws/cloudformation/csye6225-lambda-aws-cf-create-serverless-stack.sh
@@ -0,0 +1,25 @@
+read -p 'Enter stack name: ' STACK_NAME
+echo $STACK_NAME
+
+function=ResetPassword
+domain_name=$(aws route53 list-hosted-zones --query 'HostedZones[0].Name' --output text)
+bucket_name="lambda."$domain_name"csye6225.com"
+lambda_role=$(aws iam get-role --role-name LambdaExecutionRole --query Role.Arn --output text)
+file_name=$(aws s3api list-objects --bucket $bucket_name --query Contents[0].Key --output text)
+
+STACK_ID=$(\aws cloudformation create-stack --stack-name ${STACK_NAME} \
+--template-body file://csye6225-lambda-aws-cf-serverless.json \
+--parameters ParameterKey=BucketName,ParameterValue=${bucket_name} ParameterKey=FunctionName,ParameterValue=${function} ParameterKey=LambdaRole,ParameterValue=${lambda_role} ParameterKey=FileName,ParameterValue=${file_name} ParameterKey=DomainName,ParameterValue=${domain_name} \
+--capabilities CAPABILITY_IAM \
+--capabilities CAPABILITY_NAMED_IAM \
+| jq -r .StackId \
+)
+
+echo "Waiting on ${STACK_ID} create completion.."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
+if [ $? -ne 0 ]; then
+	echo "Stack ${STACK_NAME} creation failed!"
+    exit 1
+else
+    echo "Stack ${STACK_NAME} created successfully!"
+fi
